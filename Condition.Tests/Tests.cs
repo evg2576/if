d8275bc01f7a43846483708b78155c5feef28d22
Dzobko Ruslan <Ruslan_Dzobko@epam.com>
2020-04-16 21:19:21 +0300

    Добавьте файлы проекта.

---
+++ Condition.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Condition.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Pass()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Condition/Condition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Condition
+{
+    public static class Condition
+    {
+        /// <summary>
+
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int Task1(int n)
+        {
+            //TODO :Delete code below and write your own solution 
+            throw new NotImplementedException();
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+
+        public static int Task2(int n)
+        {
+            //TODO :Delete code below and write your own solution 
+            throw new NotImplementedException();
+
+        }
+    }
+}
