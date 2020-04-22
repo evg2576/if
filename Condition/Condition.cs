3c3a7ef618b587dc1b3b1064ead58a651b9d0c0b
Ruslan Slim <dnepr.ukrainec@gmail.com>
2020-04-22 09:20:27 +0000

    Add summary

--- Condition/Condition.cs
+++ Condition/Condition.cs
@@ -5,20 +5,20 @@ namespace Condition
     public static class Condition
     {
         /// <summary>
-
+        /// Implement code according to description of  task 1
         /// </summary>        
         public static int Task1(int n)
         {
-            //TODO :Delete code below and write your own solution 
+            //TODO :Delete line below and write your own solution 
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// 
+        /// Implement code according to description of  task 2
         /// </summary>  
         public static int Task2(int n)
         {
-            //TODO :Delete code below and write your own solution 
+            //TODO :Delete  line  below and write your own solution 
             throw new NotImplementedException();
         }
     }
